50c083632a62640decc5fa72eaff3aee323de20c
Sanjay Minukuri <141247388+MinukuriSanjay@users.noreply.github.com>
2024-09-19 21:19:41 +0530

    Setting up React Router

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./pages/login/Login";
+import ProfileUpdate from "./pages/profileUpdate/ProfileUpdate";
+import Chat from "./pages/chat/chat";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useContext, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/fireBase";
+import { AppContext } from "./context/AppContext";
+
+const App = () => {
+  const navigate = useNavigate();
+  const { loadUserData } = useContext(AppContext);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate("/chat");
+        await loadUserData(user.uid);
+      } else {
+        navigate("/");
+      }
+    });
+  }, []);
+  return (
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/profile" element={<ProfileUpdate />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
